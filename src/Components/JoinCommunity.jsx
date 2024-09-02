6a28539e5ff36c6379bf33f51e26bf19eec24a25
karthikaprakash08 <karthikaprakash008@gmail.com>
2024-09-02 22:17:30 +0530

    join whatsapp

--- src/Components/JoinCommunity.jsx
+++ src/Components/JoinCommunity.jsx
@@ -20,19 +20,31 @@ const JoinCommunity = () => {
       >
         All HR / Management students can be part of WOW HR Community. Follow the below instructions:                                                                                                                          
         You can fill your LinkedIn details by clicking on the "Join Our Community" button below and generate your complimentary WOW HR membership badge.                                      
-        Post your badge on LinkedIn and tag WOW HR.
+        Post your badge on LinkedIn and tag WOW HR. Also join our whatsapp group.
       </div>
       
-      <a
-        href="https://wowhr.in/badge/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="bg-darkBlue font-jost text-white text-[.6rem] md:text-base xl:text-sm xl:hover:shadow-lg px-2 md:px-8 xl:px-6 min-w-24 xl:min-w-36 py-2 xl:py-3 font-medium rounded-md xl:hover:bg-[#089adec1] xl:hover:text-white transition-all duration-200 xl:hover:scale-[1.02] xl:hover:translate-y-[-.1rem]"
-        data-aos="zoom-in"
-        data-aos-duration="1000"
-      >
-        Join Our Community
-      </a>
+      <div className="flex space-x-4">
+        <a
+          href="https://wowhr.in/badge/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-darkBlue font-jost text-white text-[.6rem] md:text-base xl:text-sm xl:hover:shadow-lg px-2 md:px-8 xl:px-6 min-w-24 xl:min-w-36 py-2 xl:py-3 font-medium rounded-md xl:hover:bg-[#089adec1] xl:hover:text-white transition-all duration-200 xl:hover:scale-[1.02] xl:hover:translate-y-[-.1rem]"
+          data-aos="zoom-in"
+          data-aos-duration="1000"
+        >
+          Join Our Community
+        </a>
+        <a
+          href="https://chat.whatsapp.com/Gnfxll2wN3Y5Mg8kJowpAj"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-darkBlue font-jost text-white text-[.6rem] md:text-base xl:text-sm xl:hover:shadow-lg px-2 md:px-8 xl:px-6 min-w-24 xl:min-w-36 py-2 xl:py-3 font-medium rounded-md xl:hover:bg-[#089adec1] xl:hover:text-white transition-all duration-200 xl:hover:scale-[1.02] xl:hover:translate-y-[-.1rem]"
+          data-aos="zoom-in"
+          data-aos-duration="1000"
+        >
+          Join WhatsApp Group
+        </a>
+      </div>
     </section>
   );
 };
